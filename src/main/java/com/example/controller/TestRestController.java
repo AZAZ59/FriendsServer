84c32221d9715c4425fa31b1bde04509797c7e33
Stepanov Sergey <step-srg@yandex.ru>
2016-03-27 14:07:57 +0400

    Deploy

--- src/main/java/com/example/controller/TestRestController.java
+++ src/main/java/com/example/controller/TestRestController.java
@@ -3,6 +3,9 @@ package com.example.controller;
 import com.example.entity.Data;
 import com.example.entity.PlayerRoom;
 import com.example.entity.UserData;
+import com.example.service.RoomService;
+import com.example.service.UserDataService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,18 +19,48 @@ import java.util.concurrent.atomic.AtomicLong;
 public class TestRestController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
+    @Autowired
+    private RoomService roomService;
+    @Autowired
+    private UserDataService userService;
 
     @RequestMapping("/greeting")
     public Data greeting(@RequestParam(value = "data", defaultValue = "World") String name) {
-        PlayerRoomController room= new PlayerRoomController();
-        AddAndGetUserController user= new AddAndGetUserController();
+        PlayerRoom room1= new PlayerRoom();
+        PlayerRoom room2= new PlayerRoom();
 
-        UserData user1=user.login("user1@asd.ru",123L);
-        UserData user2=user.login("user2@asd.ru",234L);
+        UserData user1=new UserData();
+        UserData user2=new UserData();
+        UserData user3=new UserData();
 
-        PlayerRoom room1 = room.create(user1.getId(), true,"place1");
-        room.addPlayer(room1.getId(),user2.getId());
-        room.create(user.login("user3@qwe.ru",345L).getId(),true,"place2");
+        user1.setEmail("user1@email.ru");
+        user1.setRoom(room1);
+
+        user2.setEmail("user2@email.ru");
+        user2.setRoom(room1);
+
+        user3.setEmail("user3@email.ru");
+        user3.setRoom(room2);
+
+        room1.setPlace("place1");
+        room1.setOpen(true);
+        room1.setStartTime(System.currentTimeMillis());
+        room1.setCreator(user1);
+        room1.getUsers().add(user1);
+        room1.getUsers().add(user2);
+
+        room1.setPlace("place2");
+        room1.setOpen(true);
+        room1.setStartTime(System.currentTimeMillis()+123456789L);
+        room1.setCreator(user3);
+        room1.getUsers().add(user3);
+
+        userService.persist(user1);
+        userService.persist(user2);
+        userService.persist(user3);
+
+        roomService.persist(room1);
+        roomService.persist(room2);
 
         return new Data(counter.incrementAndGet(),String.format(template, name));
     }
