16acb4f5dae25f658cf5291bffb94c422971138b
Stepanov Sergey <step-srg@yandex.ru>
2016-03-27 13:42:55 +0400

    Deploy

--- src/main/java/com/example/controller/TestRestController.java
+++ src/main/java/com/example/controller/TestRestController.java
@@ -1,6 +1,8 @@
 package com.example.controller;
 
 import com.example.entity.Data;
+import com.example.entity.PlayerRoom;
+import com.example.entity.UserData;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +19,16 @@ public class TestRestController {
 
     @RequestMapping("/greeting")
     public Data greeting(@RequestParam(value = "data", defaultValue = "World") String name) {
+        PlayerRoomController room= new PlayerRoomController();
+        AddAndGetUserController user= new AddAndGetUserController();
+
+        UserData user1=user.login("user1@asd.ru",null);
+        UserData user2=user.login("user2@asd.ru",null);
+
+        PlayerRoom room1 = room.create(user1.getId(), true,"place1");
+        room.addPlayer(room1.getId(),user2.getId());
+        room.create(user.login("user3@qwe.ru",null).getId(),true,"place2");
+
         return new Data(counter.incrementAndGet(),String.format(template, name));
     }
 }
